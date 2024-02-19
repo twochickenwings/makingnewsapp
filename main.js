5f241b7ce863f4082e1a5ff68992cb8cbdf2c091
twochickenwings <156501216+twochickenwings@users.noreply.github.com>
2024-02-19 04:08:21 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const API_KEY=`dd80fe8681ae4aa49d81b93b700e1d06`
+let news = []
+const getLatestNews = async ()=>{
+    const url = new URL (
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        ); 
+    const response = await fetch(url);
+    const data = await response.json();
+    news = data.articles;
+    console.log("dddd",news);
+};
+getLatestNews();
