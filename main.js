2a7f45e4dee8f7463fee1cf283bbda801aedf265
twochickenwings <156501216+twochickenwings@users.noreply.github.com>
2024-02-19 04:30:25 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY=`dd80fe8681ae4aa49d81b93b700e1d06`
 let news = []
 const getLatestNews = async ()=>{
     const url = new URL (
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://shimmering-buttercream-e6a4db.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
         ); 
     const response = await fetch(url);
     const data = await response.json();
